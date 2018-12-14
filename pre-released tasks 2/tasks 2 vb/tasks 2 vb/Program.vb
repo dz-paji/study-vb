fc7cefc2da5b7b411b81a69cd35af5123a2d23d9
dz-paji <zhangqian2002@outlook.com>
2018-12-14 14:03:38 +0800

    another taks added

---
+++ pre-released tasks 2/tasks 2 vb/tasks 2 vb/Program.vb
@@ -0,0 +1,22 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        'Console.WriteLine("Hello World!")
+        ' task 1
+        Dim inputWeightArray(29) As Double
+        ' declear an array called inputWeightArray that has a double data type
+        Dim inputNameArray(29) As String
+        ' declear an array called inputNameArray that has a string data type
+        Dim systemValidationDouble As Double
+        ' declear a variable called systemValidationDouble that has a double data type.
+        Dim i As Int32
+        ' declear a counter which will be used in for loop.
+
+        For i = 0 To 29
+            'use for loop to let user input their data.
+        Next
+        Console.WriteLine("Please enter your name")
+
+    End Sub
+End Module
